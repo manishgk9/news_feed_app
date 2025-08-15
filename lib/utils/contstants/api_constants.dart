b702f261a6405a387f23d0177188d61ae817aa35
Manishgk9 <emanish365@gmail.com>
2025-08-15 13:16:44 +0530

    initial commit

--- lib/utils/contstants/api_constants.dart
+++ lib/utils/contstants/api_constants.dart
@@ -1,6 +1,6 @@
 class ApiConstants {
   static const baseUrl = 'https://newsapi.org/v2';
-  static const apiKey = '1ca0d2939faa41249b2ec7c97fb4868c';
+  static const apiKey = '';
   static const everythingEndpoint = '/everything';
   static const int pageSize = 20;
 }
